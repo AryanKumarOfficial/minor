3e602a8b6cb96869e68bea67024aec210970518f
Aryan Kumar <aryanak9163@gmail.com>
2023-12-20 23:20:56 +0530

    Refactor footer component and update opacity in Navbar.css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,44 +4,49 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
     return (
         <footer className='footer'>
-                <div className="wrapper">
-                    <div className="quick">
-                        <h5>
-                            Quick Links
-                        </h5>
-                        <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to="/">Contact</Link></li>
-                            <li><Link to="/">Contact</Link></li>
-                            <li><Link to="/">Contact</Link></li>
-                            <li><Link to="/">Contact</Link></li>
-                        </ul>
-                    </div>
-                    <div className="quick">
-                        <h5>
-                            Quick Links
-                        </h5>
-                        <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to="/">Contact</Link></li>
-                            <li><Link to="/">Contact</Link></li>
-                            <li><Link to="/">Contact</Link></li>
-                            <li><Link to="/">Contact</Link></li>
-                        </ul>
-                    </div>
-                    <div className="quick">
-                        <h5>
-                            Quick Links
-                        </h5>
-                        <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to="/">Contact</Link></li>
-                            <li><Link to="/">Contact</Link></li>
-                            <li><Link to="/">Contact</Link></li>
-                            <li><Link to="/">Contact</Link></li>
-                        </ul>
-                    </div>
+            <div className="wrapper">
+                <div className="quick">
+                    <h5>
+                        Quick Links
+                    </h5>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/">Contact</Link></li>
+                        <li><Link to="/">Contact</Link></li>
+                        <li><Link to="/">Contact</Link></li>
+                        <li><Link to="/">Contact</Link></li>
+                    </ul>
                 </div>
+                <div className="quick">
+                    <h5>
+                        Quick Links
+                    </h5>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/">Contact</Link></li>
+                        <li><Link to="/">Contact</Link></li>
+                        <li><Link to="/">Contact</Link></li>
+                        <li><Link to="/">Contact</Link></li>
+                    </ul>
+                </div>
+                <div className="quick">
+                    <h5>
+                        Quick Links
+                    </h5>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/">Contact</Link></li>
+                        <li><Link to="/">Contact</Link></li>
+                        <li><Link to="/">Contact</Link></li>
+                        <li><Link to="/">Contact</Link></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="copy bg-[#05224c] w-full text-center">
+                <p>
+                    &copy; {new Date().getFullYear()} All Rights Reserved
+                </p>
+            </div>
         </footer>
     );
 };
