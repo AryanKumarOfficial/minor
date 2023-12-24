9b6a1d027a9b6cfb55e92b427822e0040569f72a
Aryan Kumar <aryanak9163@gmail.com>
2023-12-25 01:10:03 +0530

    Refactor unauth middleware to redirect to dashboard if token exists

--- src/middleware/unauth.js
+++ src/middleware/unauth.js
@@ -3,9 +3,8 @@ import { useNavigate } from "react-router-dom";
 export default ({ children }) => {
     const navigate = useNavigate();
     const token = localStorage.getItem('token');
-    const user = JSON.parse(localStorage.getItem('user'));
-    if (!token || !user) {
-        navigate('/user/login', { replace: true });
+    if (token) {
+        navigate('/user/dashboard', { replace: true });
     }
     return children;
 }
