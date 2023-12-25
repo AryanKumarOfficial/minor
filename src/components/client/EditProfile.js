2e48602e84fc57f24afe318325b6a41583862f4f
Aryan Kumar <aryanak9163@gmail.com>
2023-12-25 17:14:58 +0530

    Refactor dashboard and profile components

---
+++ src/components/client/EditProfile.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+import { CgClose } from "react-icons/cg";
+const EditProfile = ({ handleClose, user }) => {
+    useEffect(() => {
+        // When the component is rendered, add 'no-scroll' class to the body
+        document.body.classList.add('no-scroll');
+        console.log(user);
+        // When the component is unmounted, remove 'no-scroll' class from the body
+        return () => {
+            document.body.classList.remove('no-scroll');
+        };
+    }, []); // Empty dependency array ensures this runs on mount and unmount only
+
+    const [formData, setFormData] = useState({
+        fname: user.fname,
+        lname: user.lname,
+        email: user.email,
+        phone: user.phone,
+        address: user.address,
+    });
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value });
+    }
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Add logic to update the user's profile
+        handleClose();
+    };
+
+    return (
+        <div className="fixed inset-0 flex items-center justify-center bg-opacity-50 bg-gray-500 backdrop-filter backdrop-blur-sm">
+            <div className="bg-white w-1/4 rounded-lg p-8 relative">
+                <h2 className="text-2xl font-bold mb-4">Edit Profile</h2>
+                <div className="absolute top-4 right-4">
+                    <CgClose size={25} onClick={handleClose} className="text-gray-500 font-bold  hover:text-gray-700 cursor-pointer" />
+                </div>
+                <form onSubmit={handleSubmit} >
+                    <div className="mb-4">
+                        <label htmlFor="name" className="block text-gray-700">First Name:</label>
+                        <input type="text" id="name" value={formData.fname} onChange={handleChange} className="border border-gray-300 rounded-md px-3 py-2 w-full" />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="name" className="block text-gray-700">Last Name:</label>
+                        <input type="text" id="name" value={formData.lname} onChange={handleChange} className="border border-gray-300 rounded-md px-3 py-2 w-full" />
+                    </div>
+
+                    <div className="mb-4">
+                        <label htmlFor="email" className="block text-gray-700">Email:</label>
+                        <input type="email" id="email" value={formData.email} className="border border-gray-300 rounded-md px-3 py-2 w-full read-only:bg-gray-300" readOnly />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="phone" className="block text-gray-700">Phone:</label>
+                        <input type="text" id="phone" value={formData.phone} onChange={handleChange} className="border border-gray-300 rounded-md px-3 py-2 w-full" />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="address" className="block text-gray-700">Address:</label>
+                        <textarea id="address" value={formData.address} onChange={handleChange} className="border border-gray-300 rounded-md px-3 py-2 w-full h-20" placeholder="Enter your address" />
+                    </div>
+                    <button type="submit" className="bg-blue-500 text-white w-full px-4 py-2 rounded-md ml-auto hover:bg-blue-600">Save</button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default EditProfile;
--- src/components/client/Profile.js
+++ src/components/client/Profile.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
-import UserContext from "../../context/client/UserContext";
+import { HiPencilAlt } from "react-icons/hi";
+import EditProfile from "./EditProfile";
 
 const Profile = () => {
     const [userDetail, setUserDetail] = useState({
@@ -7,8 +8,11 @@ const Profile = () => {
         lname: null,
         email: null,
         phone: null,
+        address: null,
+        role: null,
 
     });
+    const [edit, setEdit] = useState(false);
     const capitalize = (str) => {
         return str.charAt(0).toUpperCase() + str.slice(1);
     };
@@ -19,23 +23,41 @@ const Profile = () => {
         user = JSON.parse(user);
         setUserDetail({
             ...userDetail,
+            id: user._id,
             fname: capitalize(user.fname),
             lname: capitalize(user.lname),
             email: user.email,
             phone: user.phone,
+            address: user.address,
+            role: user.role,
         });
     }
         , []);
 
+    const handleEdit = () => {
+        // edit profile
+        // show edit profile component
+        setEdit(true);
+        // hide profile component
+    }
+
+    const handleClose = () => {
+        setEdit(false);
+    }
+
+
+
     return (
         <>
             {/* create a profile component for users */}
+
             <section
                 style={{
                     padding: "2rem",
                     margin: "2rem",
                 }}
                 className="drop-shadow-2xl flex justify-center items-center rounded-lg shadow-lg">
+
                 <div className="flex flex-col justify-center items-center">
                     <img
                         src="/images/avtar.png"
@@ -47,13 +69,33 @@ const Profile = () => {
                         <span className="font-bold">Email: {userDetail?.email || 'example@mail.com'}</span>
                     </p>
                     <p className="text-sm text-gray-500">
-                        <span className="font-bold">Phone: {null}</span>
+                        <span className="font-bold">Phone: {userDetail?.phone || null}</span>
                     </p>
                     <p className="text-sm text-gray-500">
-                        <span className="font-bold">Address:</span>
+                        <span className="font-bold">Address: {userDetail?.address || null}</span>
+                    </p>
+                    <p className="text-sm text-gray-500">
+                        <span className="font-bold">Role: {userDetail?.role || null}</span>
                     </p>
                 </div>
+                <div className="flex flex-col justify-center items-center absolute top-2 right-2">
+                    <button
+                        onClick={handleEdit}
+                        className="bg-blue-600 shadow-md shadow-gray-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                        <HiPencilAlt className="text-2xl" />
+                    </button>
+                </div>
             </section>
+
+            {
+                edit &&
+                <section id="edit" className="">
+                    <EditProfile handleClose={handleClose} user={userDetail} />
+                </section>
+            }
+
+
+
         </>
     );
 };
