73ce07249c4e90816d05933ffb23d5b06e700eab
Aryan Kumar <aryanak9163@gmail.com>
2023-12-27 16:33:33 +0530

    Adjust viewport meta tag and position of footer in responsive design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,45 +1,51 @@
 import React from 'react';
 import './Footer.css';
 import { PiPhoneCallFill } from 'react-icons/pi';
-import { IoIosMail } from "react-icons/io";
+import { IoIosMail } from 'react-icons/io';
 import { Link } from 'react-router-dom';
 import { FaLocationDot } from 'react-icons/fa6';
 import Logo from './Logo';
 
 const Footer = () => {
     return (
-        <footer className='footer'>
+        <footer className="footer">
             <div className="wrapper">
                 <div className="logo-section">
                     <Link
-                        to={'/'}
-                        className='flex flex-col items-center justify-center space-y-2 transition-all cursor-default'
+                        to="/"
+                        className="flex flex-col items-center justify-center space-y-2 transition-all cursor-default"
                     >
                         <Logo
-                            className='peer border-8 p-1 border-[#fff] rounded-full w-16 h-16 hover:scale-105 transition-all hover:border-[#07294d] shadow-2xl md:w-1/2 md:mx-auto md:my-0 md:mt-10 md:mb-10 cursor-pointer outline-emerald-700 outline-double outline-8'
+                            className="peer border-8 p-1 border-[#fff] rounded-full w-16 h-16 hover:scale-105 transition-all hover:border-[#07294d] shadow-2xl md:w-1/2 md:mx-auto md:my-0 md:mt-10 md:mb-10 cursor-pointer outline-emerald-700 outline-double outline-8"
                             fill={'#fff'}
                         />
-                        <p className='peer-hover:text-white transition-all pt-4'>
+                        <p className="peer-hover:text-white text-sm md:text-base transition-all pt-4">
                             Connecting Patients to Care, Anytime, Anywhere.
                         </p>
                     </Link>
                 </div>
                 <div className="quick">
-                    <h5>
-                        Quick Links
-                    </h5>
+                    <h5>Quick Links</h5>
                     <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/hospital">Find Hospital</Link></li>
-                        <li><Link to="/about">About Us</Link></li>
-                        <li><Link to="/privacy">Privacy Policy</Link></li>
-                        <li><Link to="/terms">Terms & Conditions</Link></li>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/hospital">Find Hospital</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About Us</Link>
+                        </li>
+                        <li>
+                            <Link to="/privacy">Privacy Policy</Link>
+                        </li>
+                        <li>
+                            <Link to="/terms">Terms & Conditions</Link>
+                        </li>
                     </ul>
                 </div>
                 <div className="contact">
-                    <h5>
-                        Get in Touch
-                    </h5>
+                    <h5>Get in Touch</h5>
                     <ul>
                         <li>
                             <Link to="tel:+918235172505">
@@ -53,7 +59,7 @@ const Footer = () => {
                         <li>
                             <Link to="mailto:support@hospitalo.com">
                                 <div className="icon">
-                                    < IoIosMail />
+                                    <IoIosMail />
                                     <span>Mail:</span>
                                 </div>
                                 <span>support@hospitalo.com</span>
@@ -61,20 +67,19 @@ const Footer = () => {
                         </li>
 
                         <li>
-                            <Link to={`http://bit.ly/hospitalo`}>
+                            <Link to="http://bit.ly/hospitalo">
                                 <div className="icon">
-                                    < FaLocationDot />
+                                    <FaLocationDot />
                                     <span>Address:</span>
                                 </div>
                                 <span>Chankaya Boy's Hostel</span>
                             </Link>
                         </li>
-
                     </ul>
                 </div>
             </div>
-            <div className="copy bg-[#05224c] w-full text-center">
-                <p>
+            <div className="copy bg-[#05224c] w-full text-center py-2">
+                <p className="text-xs md:text-sm text-white">
                     &copy; {new Date().getFullYear()} All Rights Reserved
                 </p>
             </div>
