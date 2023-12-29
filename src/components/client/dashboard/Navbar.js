99b322120a493f2dc07160cf5c632a30d3528037
Aryan Kumar <aryanak9163@gmail.com>
2023-12-30 02:03:47 +0530

    Refactor Navbar component styles

--- src/components/client/dashboard/Navbar.js
+++ src/components/client/dashboard/Navbar.js
@@ -32,8 +32,8 @@ const NavbarComponent = () => {
     };
 
     return (
-        <nav className='bg-gray-800 p-4 md:p-6'>
-            <div className='container mx-auto md:mx-4 flex flex-wrap items-center justify-between md:grid md:grid-cols-2 md:w-screen md:justify-between'>
+        <nav className='bg-gray-800 flex p-4  md:py-6'>
+            <div className='container mx-auto md:mx-4 flex flex-wrap items-center justify-between md:grid md:grid-cols-2 md:w-screen md:'>
                 <Link className='navbar-brand flex justify-center items-center text-white hover:text-gray-500 transition-colors duration-700 md:flex md:justify-start md:w-full' to='/'>
                     <Logo
                         className={`w-14 h-14 mr-2 -ml-2 md:block hover:animate-bounce transition-all duration-700 ease-in-out`}
@@ -49,8 +49,8 @@ const NavbarComponent = () => {
                     {!isMenuOpen ? <RiMenuLine size={35} className='text-2xl' /> : <MdClose size={35} className='text-2xl' />}
                 </button>
 
-                <div className={`md:flex flex-col md:flex-row flex-wrap flex-grow justify-end w-full ${isMenuOpen ? 'block' : 'hidden'} transition-all `}>
-                    <ul className={`flex flex-col md:flex-row flex- justify-start items-center space-x-0 md:space-x-0 md:space-y-0 content-start ${isMenuOpen ? 'block' : 'hidden'} md:flex py-2 md:relative md:left-16`}>
+                <div className={`md:flex justify-end ${isMenuOpen ? 'block' : 'hidden'} transition-all `}>
+                    <ul className={`flex flex-col md:flex-row  justify-end items-center content-start ${isMenuOpen ? 'block' : 'hidden'} md:flex py-2 `}>
                         <NavLink
                             isMenuOpen={isMenuOpen}
                             to='/user/dashboard/appointments'
@@ -75,13 +75,15 @@ const NavbarComponent = () => {
                         >
                             Profile
                         </NavLink>
+                        <li className='w-full flex py-2 md:flex md:p-0'>
+                            <button
+                                className={`flex justify-center items-center text-white hover:text-gray-500 transition-colors duration-700 ${isMenuOpen ? 'block' : 'hidden'} md:flex md:justify-start md:items-center md:p-0 md:px-4`}
+                                onClick={handleLogout}
+                            >
+                                <RiLogoutBoxLine className='mr-1' size={20} /> Logout
+                            </button>
+                        </li>
                     </ul>
-                    <button
-                        className='flex flex-wrap items-center float-left text-white hover:text-gray-500 transition-colors duration-700 focus:outline-none md:flex md:justify-center md:items-center md:py-2 md:px-4 md:absolute md:right-0 md:mr-4 md:mt-0'
-                        onClick={handleLogout}
-                    >
-                        <RiLogoutBoxLine className='mr-1' size={20} /> Logout
-                    </button>
                 </div>
             </div>
         </nav>
