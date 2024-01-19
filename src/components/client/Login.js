450ad2665c9cbaeaa9a39605dcf1bfdd06d6b6d0
Aryan Kumar <aryanak9163@gmail.com>
2024-01-19 18:30:09 +0530

    Refactor login component and remove captcha functionality

--- src/components/client/Login.js
+++ src/components/client/Login.js
@@ -1,24 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import './Login.css';
-import { Link, redirect, useNavigate } from 'react-router-dom';
-import toast, { } from 'react-hot-toast';
+import toast from 'react-hot-toast';
 import { HiAtSymbol, HiFingerPrint } from "react-icons/hi";
 import { useDispatch, useSelector } from 'react-redux';
+import { Link, redirect, useNavigate } from 'react-router-dom';
 import { loginUser } from '../../Redux/slices/userSlice';
-import ReCAPTCHA from 'react-google-recaptcha';
 import Layout from './Layout';
+import './Login.css';
 
 const UserLogin = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const captchaRef = React.useRef();
-
     // const { loginUser, user, showToast } = useContext(UserContext);
     // states
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [disabled, setDisabled] = useState(true);
-    const [captchaToken, setCaptchaToken] = useState('');
     // redux states
     const { isAuthenticated, loading } = useSelector(state => state.user);
 
@@ -50,45 +46,11 @@ const UserLogin = () => {
 
     }, [email, password, isAuthenticated, navigate]);
 
-    const verifyCaptcha = async (captchaToken) => {
-        const req = await fetch(`${process.env.REACT_APP_API_HOST}/user/verify-captcha`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ captchaToken }),
-        });
-        const data = await req.json();
-        return data;
-    }
 
     const handleSubmit = async (e) => {
-        let verified;
         e.preventDefault();
-        if (captchaToken) {
-            verified = await verifyCaptcha(captchaToken);
-            captchaRef?.current?.reset();
-        }
-        else {
-            toast.error('Please verify captcha', {
-                duration: 2000,
-                position: 'top-center',
-                style: {
-                    background: '#d2bfb2',
-                    color: '#000',
-                    border: '2px solid #000',
-                    padding: '16px',
-                    zIndex: 1,
-                },
-                iconTheme: {
-                    primary: '#000',
-                    secondary: '#FFFAEE',
-                },
-            });
-            return;
-        }
+        try {
 
-        if (verified.success) {
             let credentials = { email, password };
             dispatch(loginUser(credentials)).then((result) => {
                 if (result?.payload?.success) {
@@ -131,10 +93,9 @@ const UserLogin = () => {
                     });
                 }
             });
+        } catch (error) {
 
-        }
-        else {
-            toast.error('Please verify captcha', {
+            toast.error('Invalid Credentials', {
                 duration: 2000,
                 position: 'top-center',
                 style: {
@@ -149,7 +110,9 @@ const UserLogin = () => {
                     secondary: '#FFFAEE',
                 },
             });
+
         }
+
 
     }
 
@@ -207,16 +170,6 @@ const UserLogin = () => {
                     <div className="flex justify-end items-start pb-1 w-full">
                         <Link to='/user/forgot-password' className='text-sm text-maroon hover:underline transition-all duration-500'>Forgot Password?</Link>
                     </div>
-                    <ReCAPTCHA
-                        sitekey={`${process.env.REACT_APP_RECAPTCHA_SITE_KEY}`}
-                        className='my-2'
-                        onChange={(token) => {
-                            setCaptchaToken(token)
-                        }}
-                        onExpired={() => setCaptchaToken('')}
-                        onErrored={() => setCaptchaToken('')}
-                        ref={captchaRef}
-                    />
                     <button
                         type="submit"
                         className="rounded-lg w-full bg-maroon text-white font-bold  hover:bg-opacity-80 transition-all duration-300 ease-in-out disabled:!cursor-not-allowed disabled:!bg-[#d2bfb2] disabled:hover:!text-white disabled:hover:!shadow-none"
